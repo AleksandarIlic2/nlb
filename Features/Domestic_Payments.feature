4e1e2ef6c1b0bed6795448da2f8baec73faf1f56
Aleksandar <aca.ilic024@gmail.com>
2026-02-04 12:34:36 +0100

    Nekoliko testova sitne ispravke, Payment in Future test..

--- Features/Domestic_Payments.feature
+++ Features/Domestic_Payments.feature
@@ -160,3 +160,68 @@ Feature: Domestic_Payments
     Examples:
       | rowindex |
       |        2 |
+
+
+  @Domestic_Payments_In_Future_[WEB]
+  Scenario Outline: Domestic_Payments_In_Future_[WEB]
+    #TODO: Kada se resi konacnos ta je sa tarifama, dodati i proveru tarife u okviru placanja u istoj/drugoj banci
+    Given Open Login page
+    And Change language to English
+    And Login to the page using user from Excel "<rowindex>" columnName "username"
+    And Click on element by text "Payments"
+    And Click on element by text "Domestic payment"
+    And Assert today date in Payment date in payment review
+    And Assert element by class "icon-calendar-today"
+    And Assert checkbox Urgent payment is "checked"
+    #TODO: Dodati authorized kad bude bilo..
+    And Click on element by containing class "icon-chevron-down" with index "1"
+    And Click on element by containing text from Excel "<rowindex>" columnName "current_account_2_bban"
+    And Click on element by text "Select from list "
+    Then Click on element by text " PERA "
+    And Assert "First and last name / Company name" in pay or transfer screen is from excel "<rowindex>" columnName "user_name_for_payment_review"
+    And Assert "Street and street number" in pay or transfer screen is from excel "<rowindex>" columnName "user_street_for_payment_review"
+    And Assert "City" in pay or transfer screen is from excel "<rowindex>" columnName "user_city_for_payment_review"
+    And Assert "Account number" in pay or transfer screen is from excel "<rowindex>" columnName "user_bban_for_payment_review"
+    #And Assert "Address" in payment review is from excel "<rowindex>" columnName "user_address_for_payment_review"
+    And Enter text "73" in field by xPath "//label[text()='Payment amount ']/following-sibling::div//input"
+    And Assert "Payment amount" in pay or transfer screen is from excel "<rowindex>" columnName "user_amount_for_payment_review"
+    And Assert amount currency is displayed in "RSD"
+    And Click on element by class "icon-calendar-today"
+    And Click on date 30 days in the future in second payment screen
+    And Assert date 30 days in future in payment screen
+    And Assert checkbox Urgent payment is "notChecked"
+    #TODO: KADA RESE BUG, DODAIT PROVERU DA NIJE MOGUCE KLIKNUTI NA CHECKBOX KADA JE PLACANJE U BUDUCNOSTI
+    Then Click on element by text " Confirm "
+    Then Assert element by text "Purpose" has following sibling "dd" with text "TRANSACTIONS BY ORDER OF CITIZENS"
+    Then Assert element by text "Purpose code" has following sibling "dd" with text "289"
+    And Assert date 30 days in future in payment review
+
+     #provera sa dve razliite funkcije za Frist and last name/ Company name i account number"
+    And Assert "First and last name / Company name" in payment review is from excel "<rowindex>" columnName "username_debtor_for_payment_review"
+    Then Assert element by text "First and last name / Company name" has following sibling "dd" with text from Excel "<rowindex>" columnName "username_debtor_for_payment_review" and index 0
+    And Assert "Account number" in payment review is from excel "<rowindex>" columnName "current_account_2_bban"
+    Then Assert element by text "Account number" has following sibling "dd" with text from Excel "<rowindex>" columnName "current_account_2_bban" and index 0
+
+    Then Assert element by text "First and last name / Company name" has following sibling "dd" with text from Excel "<rowindex>" columnName "user_name_for_payment_review" and index 1
+    And Assert second "First and last name / Company name" in payment review is from excel "<rowindex>" columnName "user_name_for_payment_review"
+    And Assert second "Account number" in payment review is from excel "<rowindex>" columnName "user_bban_for_payment_review"
+    Then Assert element by text "Account number" has following sibling "dd" with text from Excel "<rowindex>" columnName "user_bban_for_payment_review" and index 1
+
+    Then Assert element by text "Address" has following sibling "dd" with text from Excel "<rowindex>" columnName "user_address_for_payment_review" and index 1
+    And Assert element by text "Cancel"
+    And Assert element by text "Back"
+    And Assert element by text " Confirm "
+
+    Then Click on element by text " Confirm "
+    And Try to assert that payment is "Success" and send command to Authorize method
+    And Check if authorization is needed and complete payment with account iban from Excel "<rowindex>" columnName "united_kingdom_iban" amount "10.00" and currency "EUR" with message "Success"
+    And Click on tab "Payments" from main sidebar
+    Then Assert element by tag "div" containing text "Payments"
+    And Select account from Excel "<rowindex>" columnName "current_account_2_bban" in Payments tab
+    And Click on element by containing text "Upcoming payments"
+    #TODO: Transakcjie se ne pojavljuju, da li zato sto nije urgent payment?
+    #Kad se pojavi ta transakcija assertovati je...
+    
+    Examples:
+      | rowindex |
+      |        1 |
--- src/main/java/si/nlb/testautomation/NLBTestAutomation/Test/Steps.java
+++ src/main/java/si/nlb/testautomation/NLBTestAutomation/Test/Steps.java
@@ -2114,6 +2114,14 @@ public class Steps {
         assertEquals(expectedValue, elementForAssert.getAttribute("textContent").trim());
     }
 
+    @And("Assert {string} in pay or transfer screen is from excel {string} columnName {string}")
+    public void assertInPayOrTransferScreenIsFromExcelColumnName(String text, String rowindex, String columnName) throws Throwable {
+        String expectedValue = DataManager.getDataFromHashDatamap(rowindex, columnName);
+        String xPathForElementAssert = "//label[contains(text(),'" + text + "')]/following-sibling::div//input";
+        WebElement elementForAssert = SelectByXpath.CreateElementByXpath(xPathForElementAssert);
+        assertEquals(expectedValue.replace("-", ""), elementForAssert.getAttribute("value").trim());
+    }
+
     @And("Assert {string} in payment review is from excel {string} columnName {string}")
     public void assertInPaymentReviewIsFromExcelColumnName(String text, String rowindex, String columnName) throws Throwable {
         String expectedValue = DataManager.getDataFromHashDatamap(rowindex, columnName);
@@ -2121,6 +2129,7 @@ public class Steps {
         WebElement elementForAssert = SelectByXpath.CreateElementByXpath(xPathForElementAssert);
         assertEquals(expectedValue, elementForAssert.getAttribute("textContent").trim());
     }
+
 
     @And("Assert second {string} in payment review is from excel {string} columnName {string}")
     public void assertSecondInPaymentReviewIsFromExcelColumnName(String text, String rowindex, String columnName) throws Throwable {
@@ -2141,9 +2150,9 @@ public class Steps {
     @And("Assert today date in Payment date in payment review")
     public void assertTodayDateInPaymentDateInPaymentReview() throws Throwable {
         String expectedDate = ActionApiHelpers.getTodayDate();
-        String xPathForElementAssert = "//dt[contains(text(),'Payment date')]//following-sibling::dd";
+        String xPathForElementAssert = "//label[normalize-space()='Payment date']/following-sibling::div//input";
         WebElement elementForAssert = SelectByXpath.CreateElementByXpath(xPathForElementAssert);
-        assertEquals(expectedDate, elementForAssert.getAttribute("textContent"));
+        assertEquals(expectedDate, elementForAssert.getAttribute("value"));
     }
 
     @And("Select account from Excel {string} columnName {string} in Payments tab")
@@ -2875,9 +2884,11 @@ public class Steps {
     @And("Assert date {int} days in future in payment review")
     public void assertDateDaysInFutureInPaymentReview(int daysInFuture) throws Throwable {
         String expectedDate = ActionApiHelpers.getTodayDatePlusXDaysInFormat(daysInFuture, "dd.MM.YYYY");
-        String xPathForElementAssert = "//*[contains(text(),'Payment date')]//following-sibling::*";
+        String xPathForElementAssert = "//dt[contains(text(),'Payment date')]/following-sibling::dd";
+        //String xPathForElementAssert = "//label[contains(text(),'Payment date')]/following-sibling::div//input";
         WebElement elementForAssert = SelectByXpath.CreateElementByXpath(xPathForElementAssert);
-        assertEquals(expectedDate, elementForAssert.getAttribute("textContent"));
+      //  assertEquals(expectedDate, elementForAssert.getAttribute("value"));
+        assertEquals(expectedDate, elementForAssert.getText());
     }
 
     @And("Assert that payment under key {string} from txt file has date {int} days in future")
@@ -3389,14 +3400,14 @@ public class Steps {
     @And("Assert element by text {string} has following sibling {string} with text from Excel {string} columnName {string}")
     public void assertElementByTextHasFollowingSiblingWithTextFromExcelColumnName(String text, String followingSiblingTag, String rowindex, String columnName) throws Throwable {
         String followingSiblingText = DataManager.getDataFromHashDatamap(rowindex, columnName);
-        WebElement element = SelectByXpath.CreateElementByXpathTextFollowingSibling(text, followingSiblingTag);
-        assertEquals(followingSiblingText, element.getAttribute("textContent"));
+        List<WebElement> element = SelectByXpath.CreateElementByXpathTextFollowingSibling(text, followingSiblingTag);
+        assertEquals(followingSiblingText, element.get(0).getAttribute("textContent"));
     }
 
     @And("Assert element by text {string} has following sibling {string} with text {string}")
     public void assertElementByTextHasFollowingSiblingWithText(String text, String followingSiblingTag, String expectedText) throws Throwable {
-        WebElement element = SelectByXpath.CreateElementByXpathTextFollowingSibling(text, followingSiblingTag);
-        assertEquals(expectedText, element.getAttribute("textContent"));
+        List<WebElement> elements = SelectByXpath.CreateElementByXpathTextFollowingSibling(text, followingSiblingTag);
+        assertEquals(expectedText, elements.get(0).getAttribute("textContent"));
     }
 
     @And("Use mobile app to complete payment using wrong pin")
@@ -3426,6 +3437,7 @@ public class Steps {
         String text = DataManager.getDataFromHashDatamap(rowindex, columnName);
         String xPath = "//*[contains(text(),'" + text + "')]";
         WebElement element = SelectByXpath.CreateElementByXpath(xPath);
+        System.out.println("EXCEL VALUE:" + text);
         hp.ClickOnElement(element);
     }
 
@@ -10958,7 +10970,6 @@ public class Steps {
 
     @And("Assert transaction amounts are between {string} and {string}")
     public void assertTransactionAmountsAreBetween(String fromStr, String toStr) throws Throwable {
-
         double from = Double.parseDouble(fromStr.replace(",", "."));
         double to   = Double.parseDouble(toStr.replace(",", "."));
 
@@ -10968,35 +10979,38 @@ public class Steps {
 
         Assert.assertFalse("Nema transakcija na stranici!", amountElements.isEmpty());
 
+        Pattern p = Pattern.compile("[+-]?(?:\\d{1,3}(?:\\.\\d{3})+|\\d+)(?:[.,]\\d+)?");
+
         for (WebElement el : amountElements) {
 
             String rawText = el.getText().trim();
             if (rawText.isEmpty()) {
                 continue;
             }
-            System.out.println("RAW TEXT:" + rawText);
-            // 1. Izvući broj iz stringa
-            // Podržava formate:
-            // "-120,00", "120,00", "+120,00", "120", "-120", "120.00"
-            Pattern p = Pattern.compile("([+-]?[0-9]+([.,][0-9]+)?)");
+
+            System.out.println("RAW TEXT: " + rawText);
+
             Matcher m = p.matcher(rawText);
 
             if (!m.find()) {
                 Assert.fail("Nisam uspeo da izvučem amount iz: " + rawText);
             }
 
-            String numberStr = m.group(1).replace(",", ".");
-            double value = Double.parseDouble(numberStr);
+            String numberStr = m.group();
 
+            numberStr = numberStr.replace(".", "").replace(",", ".");
+
+            double value = Double.parseDouble(numberStr);
             double absValue = Math.abs(value);
 
-            // 2. Provera raspona
             Assert.assertTrue(
                     "Vrednost " + value + " (abs=" + absValue +
                             ") nije u opsegu " + from + " - " + to,
                     absValue >= from && absValue <= to
             );
         }
+
+
     }
 
     @And("Click on {string} button if displayed")
@@ -11474,4 +11488,52 @@ public class Steps {
         System.out.println(savingAccounts);
         assertEquals("Lista nije sortirana rastuće!", sorted, savingAccounts);
     }
+
+
+    @And("Assert amount currency is displayed in {string}")
+    public void assertAmountCurrencyIsDisplayedIn(String currency) throws Throwable {
+        String xPath = "//input[@nlbamountinput]/following-sibling::input[@readonly]";
+        WebElement currencyInput = SelectByXpath.CreateElementByXpath(xPath);
+        String ariaLabel = currencyInput.getAttribute("aria-label");
+        Assert.assertTrue("Amount se ne nalazi u odgovarajucoj valuti!", ariaLabel.endsWith(currency));
+
+    }
+
+    @And("Assert checkbox Urgent payment is {string}")
+    public void assertCheckboxUrgentPaymentIs(String checked) throws Throwable {
+        String xPath = "//label[.//span[normalize-space()='Urgent payment']]//input[@type='checkbox']\n";
+        WebElement urgentCheckbox = SelectByXpath.CreateElementByXpath(xPath);
+        if (checked.equals("checked"))
+            Assert.assertTrue("Urgent payment checkbox NIJE čekiran!",urgentCheckbox.isSelected());
+        else
+            Assert.assertFalse("Urgent payment checkbox je cekiran, a ne bi trebao biti!",urgentCheckbox.isSelected());
+    }
+
+    @And("Assert date {int} days in future in payment screen")
+    public void assertDateDaysInFutureInPaymentScreen(int daysInFuture) throws Throwable {
+        String expectedDate = ActionApiHelpers.getTodayDatePlusXDaysInFormat(daysInFuture, "dd.MM.YYYY");
+        String xPathForElementAssert = "//label[contains(text(),'Payment date')]/following-sibling::div//input";
+        WebElement elementForAssert = SelectByXpath.CreateElementByXpath(xPathForElementAssert);
+        assertEquals(expectedDate, elementForAssert.getAttribute("value"));
+    }
+
+    @Then("Assert element by text {string} has following sibling {string} with text from Excel {string} columnName {string} and index {int}")
+    public void assertElementByTextHasFollowingSiblingWithTextFromExcelColumnNameAndIndex(String text, String followingSiblingTag, String rowindex, String columnName, Integer index) throws Throwable {
+        String followingSiblingText = DataManager.getDataFromHashDatamap(rowindex, columnName);
+        List<WebElement> elements = SelectByXpath.CreateElementByXpathTextFollowingSibling(text, followingSiblingTag);
+        if (text.equals("Address")) {
+            String address = DataManager.getDataFromHashDatamap(rowindex, "user_address_for_payment_review");
+            String expectedValue = address
+                    .replaceAll("\\s+", " ")
+                    .trim();
+            String actualValue = elements.get(index)
+                    .getAttribute("textContent")
+                    .replaceAll("\\s+", " ")
+                    .trim();
+            assertEquals(actualValue,expectedValue);
+            return;
+        }
+        assertEquals(followingSiblingText.trim().toUpperCase(), elements.get(index).getAttribute("textContent").trim().toUpperCase());
+    }
+
 }
